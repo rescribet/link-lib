4293f5cda8971a6fc65cdc948b0a489315cb510a
Fletcher91 <thom@argu.co>
2016-12-19 12:16:38 +0100

    [IMP] Prioritise some mediatypes over others
    This should be handed over to the client application, and also requires the processors to go into separate packages, but thats a matter for another day

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import { default as LinkedDataAPI } from './LinkedDataAPI';
 const formats = require('rdf-formats-common')();
 
 import jsonapi from './processors/jsonapi';
-LinkedDataAPI.registerProcessor(jsonapi, 'application/vnd.api+json');
+LinkedDataAPI.registerProcessor(jsonapi, 'application/vnd.api+json', 1.0);
 import ntriples from './processors/ntriples';
-LinkedDataAPI.registerProcessor(ntriples, Object.keys(formats.parsers));
+const mediaTypes = Object.keys(formats.parsers);
+const rdf = mediaTypes.splice(mediaTypes.indexOf('application/rdf+xml'), 1);
+LinkedDataAPI.registerProcessor(ntriples, mediaTypes, 0.8);
+if (rdf[0]) {
+  LinkedDataAPI.registerProcessor(ntriples, 'application/rdf+xml', 1.0);
+}
 export { LinkedDataAPI };
 
 import LinkedRenderStore from './LinkedRenderStore';
