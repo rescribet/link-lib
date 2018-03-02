b0b959e60e8af56e6f5f8b75fc73aaffc10122cb
Fletcher91 <thom@argu.co>
2018-03-02 16:18:20 +0100

    [FIX] Don't modify the array

--- src/Schema.ts
+++ src/Schema.ts
@@ -105,7 +105,7 @@ export class Schema extends IndexedFormula {
 
                     return a;
                 },
-                lookupTypes,
+                [...lookupTypes],
             );
     }
 
