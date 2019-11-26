bf26a44712e71d983ebb6f65c62d83998cada3a0
Fletcher91 <thom@argu.co>
2019-11-26 17:38:45 +0100

    Use pika for builds

--- __mocks__/solid-auth-client.js
+++
@@ -1,7 +0,0 @@
-/**
- * The `rdflib` fetcher module doesn't use spec compliant implementations provided by the
- * ecosystem.. so we need to do some overrides again..
- * @module SolidAuthClient
- */
-
-module.exports = undefined;
