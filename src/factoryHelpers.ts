3fa27a6fa041aa9a816cecea06073ba8f1218775
Fletcher91 <thom@argu.co>
2020-03-27 15:21:39 +0100

    Upgrade dependencies

--- src/factoryHelpers.ts
+++ src/factoryHelpers.ts
@@ -1,9 +1,11 @@
 import rdfFactory, { Feature, Quad, Term } from "@ontologies/core";
 
 export const equals = rdfFactory.supports[Feature.identity]
-    ? (a: any, b: any): boolean => a?.id === b?.id
-    : (a: any, b: any): boolean => rdfFactory.equals(a, b);
+    ? (a: any, b: any): boolean => a === b
+    : rdfFactory.supports[Feature.idStamp]
+        ? (a: any, b: any): boolean => a?.id === b?.id
+        : (a: any, b: any): boolean => rdfFactory.equals(a, b);
 
-export const id = rdfFactory.supports[Feature.identity]
+export const id = rdfFactory.supports[Feature.idStamp]
     ? (obj?: Term | Quad | any): number => (obj as any)?.id || -1
     : (obj?: Term | Quad | any): number => rdfFactory.id(obj);
